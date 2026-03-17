d1e24bce6e12569c9f42ef88477d7b5f0006aa36
Jonathan Kohler <kohler.jonathan@gmail.com>
2026-03-17 15:38:55 -0700

    Support shape and dtype options for noise generators

--- src/noiselab/generators/relaxation.py
+++ src/noiselab/generators/relaxation.py
@@ -2,41 +2,56 @@ from __future__ import annotations
 
 import numpy as np
 from numpy.random import Generator
+from numpy.typing import DTypeLike
 
 from ._relaxation import RelaxationGenerator
 from .base import GeneratorBase
 
 
 class RelaxationProcess(GeneratorBase):
+    def __init__(
+        self,
+        *,
+        alpha: float,
+        f_min: float,
+        f_max: float,
+        mean: float = 0.0,
+        var: float = 1.0,
+        dtype: DTypeLike = np.double,
+        gaussianity: float = 10.0,
+        decay_max: float = 20.0,
+        rng: Generator | None = None,
+        seed: int | None = None,
+        cache_size: int = 65535,
+    ) -> None:
+        super().__init__(shape=1, dtype=dtype, rng=rng, seed=seed)
 
-    def __init__(self, *,
-                 alpha: float,
-                 f_min: float,
-                 f_max: float,
-                 mean: float = 0.0,
-                 gaussianity: float = 10.0,
-                 decay_max: float = 20.0,
-                 rng: Generator | None = None,
-                 seed: int | None = None,
-                 cache_size: int = 65535):
-        super().__init__(rng, seed)
-
+        self.alpha = alpha
+        self.f_min = f_min
+        self.f_max = f_max
         self.mean = mean
-        self.gen = RelaxationGenerator(alpha, f_min, f_max, gaussianity=gaussianity, decay_max=decay_max,
-                                       rng=self.rng.bit_generator, cache_size=cache_size)
+        self.var = var
+
+        self.gen = RelaxationGenerator(
+            alpha,
+            f_min,
+            f_max,
+            gaussianity=gaussianity,
+            decay_max=decay_max,
+            rng=self.rng.bit_generator,
+            cache_size=cache_size,
+        )
 
     def __getattr__(self, item: str) -> object:
         return getattr(self.gen, item)
 
     def reset(self, init: float | None = None, *, seed: int | None = None) -> None:
-        with self. _lock:
-            super().reset(seed=seed)
-            # TODO implement reset
-            raise NotImplementedError()
+        super().reset(init=init, seed=seed)
+        # TODO implement reset
+        raise NotImplementedError()
 
-    def sample(self, num: int, dt: float) -> np.ndarray:
-        with self._lock:
-            out = self.gen.get_samples(num=num, dt=dt)
+    def sample(self, num: int, *, dt: float) -> np.ndarray:
+        out = np.sqrt(self.var) * self.gen.get_samples(num=num, dt=dt)
         if self.mean:
             out += self.mean
         return out
--- src/noiselab/generators/wiener.py
+++ src/noiselab/generators/wiener.py
@@ -2,39 +2,51 @@ from __future__ import annotations
 
 import numpy as np
 from numpy.random import Generator
+from numpy.typing import DTypeLike
 
-from .base import GeneratorBase
+from .base import GeneratorBase, TShape
 
 
 class WienerProcess(GeneratorBase):
-
-    def __init__(self, *,
-                 diffusion_rate: float,
-                 mean: float = 0.0,
-                 init_var: float = 0.0,
-                 rng: Generator | None = None,
-                 seed: int | None = None):
-        super().__init__(rng=rng, seed=seed)
+    def __init__(
+        self,
+        *,
+        diffusion_rate: float,
+        mean: float = 0.0,
+        init_var: float = 0.0,
+        shape: TShape = 1,
+        dtype: DTypeLike = np.double,
+        rng: Generator | None = None,
+        seed: int | None = None,
+    ) -> None:
+        super().__init__(shape=shape, dtype=dtype, rng=rng, seed=seed)
         self.diffusion_rate = diffusion_rate
         self.mean = mean
         self.init_var = init_var
 
-        self._state = self.mean + np.sqrt(init_var) * self.rng.standard_normal(1)
+        self._state = self.mean + np.sqrt(init_var) * self.rng.standard_normal(
+            size=self.shape, dtype=self.dtype,
+        )
 
     def reset(self, init: float | None = None, *, seed: int | None = None) -> None:
-        with self._lock:
-            super().reset(init=init, seed=seed)
-            if init is not None:
-                self._state = init
-            else:
-                self._state = self.mean + np.sqrt(self.init_var) * self.rng.standard_normal(1)
+        super().reset(init=init, seed=seed)
+        if init is not None:
+            self._state = np.broadcast_to(init, self.shape)
+        else:
+            self._state = self.mean + np.sqrt(self.init_var) * self.rng.standard_normal(
+                size=self.shape, dtype=self.dtype,
+            )
 
-    def sample(self, num: int, dt: float) -> np.ndarray:
-        dx = self.rng.standard_normal(size=num, dtype=np.double)
-        with self._lock:
-            x = self._state + np.sqrt(dt * self.diffusion_rate) * np.cumsum(dx)
-            self._state = x[-1]
-            return x
+    def sample(self, num: int, *, dt: float) -> np.ndarray:
+        size = self.sample_size(num)
+        dx = self.rng.standard_normal(size=size, dtype=self.dtype)
+        x = self._state + np.sqrt(dt * self.diffusion_rate) * np.cumsum(dx, axis=0)
+        self._state = x[-1, ...]
+
+        return x
 
     def psd(self, f: np.ndarray) -> np.ndarray:
-        return self.diffusion_rate / (2 * np.pi * f)**2
+        return self.diffusion_rate / (2 * np.pi * f) ** 2
+
+    def avar(self, tau: np.ndarray) -> np.ndarray:
+        return self.diffusion_rate * tau / 3
