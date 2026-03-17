b4472911102218cf76a409bba7b4ad8667902a07
Jonathan Kohler <kohler.jonathan@gmail.com>
2026-03-17 16:28:07 -0700

    Re-organize in src layout

--- setup.py
+++ setup.py
@@ -14,17 +14,17 @@ lib_path = [
 setup(
     ext_modules=cythonize([
         Extension("noiselab.generators._markov", [
-            "noiselab/generators/_markov.pyx",
+            "src/noiselab/generators/_markov.pyx",
         ], include_dirs=[inc_path], library_dirs=lib_path),
         Extension("noiselab.generators._relaxation", [
-            "noiselab/generators/_relaxation.pyx",
+            "src/noiselab/generators/_relaxation.pyx",
         ], include_dirs=[inc_path], library_dirs=lib_path, libraries=['npymath', 'npyrandom']),
         Extension("noiselab.adev._adev", [
-            "noiselab/adev/_adev.pyx",
+            "src/noiselab/adev/_adev.pyx",
         ], include_dirs=[inc_path], library_dirs=lib_path),
         Extension("noiselab.tdigest._tdigest", [
-            "noiselab/tdigest/_tdigest.pyx",
-            "noiselab/tdigest/t-digest-c/src/tdigest.c",
-        ], include_dirs=["noiselab/tdigest/t-digest-c/src/", inc_path], library_dirs=lib_path),
+            "src/noiselab/tdigest/_tdigest.pyx",
+            "src/noiselab/tdigest/t-digest-c/src/tdigest.c",
+        ], include_dirs=["src/noiselab/tdigest/t-digest-c/src/", inc_path], library_dirs=lib_path),
     ]),
 )
